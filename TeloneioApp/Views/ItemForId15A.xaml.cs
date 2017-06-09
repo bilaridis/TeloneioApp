7372e3307edef1320f3920644c3ae7c8b32a34f3
Vasileios Valasidis <v.valasidis@signalocean.com>
2017-06-09 17:31:05 +0300

    Optimize XAML with user control

--- TeloneioApp/Views/ItemForId15A.xaml.cs
+++ TeloneioApp/Views/ItemForId15A.xaml.cs
@@ -1,28 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DomainModel.XmlModels.ID15A;
 
 namespace TeloneioApp.Views
 {
     /// <summary>
-    /// Interaction logic for ItemForId15A.xaml
+    ///     Interaction logic for ItemForId15A.xaml
     /// </summary>
     public partial class ItemForId15A : UserControl
     {
+
         public ItemForId15A()
         {
             InitializeComponent();
+        }
+
+        public ObservableCollection<GOOITEGDS> WorkItems
+        {
+            get { return (ObservableCollection<GOOITEGDS>)GetValue(WorkItemsProperty); }
+            set { SetValue(WorkItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for WorkItems.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WorkItemsProperty =
+            DependencyProperty.Register("WorkItems", typeof(ObservableCollection<GOOITEGDS>), typeof(ItemForId15A), new FrameworkPropertyMetadata(null, OnWorkItemsChanged));
+
+        private static void OnWorkItemsChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ItemForId15A me = sender as ItemForId15A;
+            if (me != null)
+            {
+                var old = e.OldValue as ObservableCollection<GOOITEGDS>;
+
+                if (old != null)
+                {
+
+                    old.CollectionChanged -= me.OnWorkCollectionChanged;
+                }
+
+                var n = e.NewValue as ObservableCollection<GOOITEGDS>;
+                me.WorkItems = n;
+
+                if (n != null)
+                {
+                    n.CollectionChanged += me.OnWorkCollectionChanged;
+                }
+            }
+        }
+
+        private void OnWorkCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+            {
+                // Clear and update entire collection
+            }
+
+            if (e.NewItems != null)
+            {
+                foreach (GOOITEGDS item in e.NewItems)
+                {
+                    // Subscribe for changes on item
+                    item.PropertyChanged += OnWorkItemChanged;
+
+                    // Add item to internal collection
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                foreach (GOOITEGDS item in e.OldItems)
+                {
+                    // Unsubscribe for changes on item
+                    item.PropertyChanged -= OnWorkItemChanged;
+
+                    // Remove item from internal collection
+                }
+            }
+        }
+
+        private void OnWorkItemChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // Modify existing item in internal collection
         }
     }
 }
